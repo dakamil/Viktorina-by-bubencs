f3adccbbb69c6e7b4b9beb959584ee5dc9e4b415
DonBublik <goroshek357yandexru@gmail.com>
2023-12-15 12:07:42 +0300

    relise 05 userModel func

--- user_model.js
+++ user_model.js
@@ -1,1 +1,20 @@
-console.log(1);
+const fs = require("fs").promises
+const {EOL} = require("os")
+
+
+
+async function readFile (){
+const read = JSON.parse(await fs.readFile(`${__dirname}/questions.json`, 'utf-8'))
+
+return  read
+}
+
+async function readAnswers (ansdwers) {
+if (ansdwers===true) {
+    return 'Вы ответили правильно! Плюс 5 баллов!'
+} else {
+    return "К сожалению, ответ не верный."
+}
+}
+
+module.exports = {readFile, readAnswers}
