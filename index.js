68b3f3a72dbc640e0390a2ad9d6debfd7d477d7b
dakamil <mr.kamiljan@gmail.com>
2023-12-15 10:54:49 +0300

    release 0: inquirer 0

--- index.js
+++ index.js
@@ -2,18 +2,3 @@ const fs = require('fs').promises;
 const { EOL } = require('os');
 
 const inquirer = require('inquirer');
-inquirer
-  .prompt([
-    { type: 'input', name: 'username', message: 'Введи имя:' },
-    {
-      type: 'list',
-      name: 'bonuses',
-      message: 'На сколько ты опоздал?',
-      choices: [
-        { name: 'Не опаздывал', value: 0 },
-        { name: 'На 2-3 минутки', value: 2 },
-        { name: 'Опоздал и не сообщил', value: 5 },
-      ],
-    },
-  ])
-  .then((answers) => console.log(answers));
